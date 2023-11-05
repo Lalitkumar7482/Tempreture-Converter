acd0f0a9086d76c65293dfc05d6bdb6cfe605f27
Lalit Kumar <lk167716@gmail.com>
2023-11-05 18:11:09 +0530

    Temperature Converter

---
+++ main.js
@@ -0,0 +1,15 @@
+function convertTemperature() {
+    var temperature = parseFloat(document.getElementById("temperature").value);
+    var unit = document.getElementById("unit").value;
+    var result;
+
+    if (unit === "celsius") {
+        result = (temperature * 9/5) + 32;
+        document.getElementById("result").innerText = `${temperature} Celsius is equal to ${result} Fahrenheit.`;
+    } else if (unit === "fahrenheit") {
+        result = (temperature - 32) * 5/9;
+        document.getElementById("result").innerText = `${temperature} Fahrenheit is equal to ${result} Celsius.`;
+    } else {
+        document.getElementById("result").innerText = "Invalid unit selected.";
+    }
+}
